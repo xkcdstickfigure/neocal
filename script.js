1d6dae91f972028065258f592a1f3e2ba0306fdf
Archie Baer <archie@abaer.dev>
2023-05-26 23:25:23 +0100

    much simpler js, no longer creates calendar object

--- script.js
+++ script.js
@@ -1,42 +1,30 @@
 const year = new Date().getFullYear()
-const calendar = []
+let day = 0
 
-let day = 1
-let date = new Date(year, 0, 1)
-
-while (date.getFullYear() === year) {
-	const month = date.getUTCMonth()
-	if (!calendar[month]) calendar[month] = []
-
-	calendar[month].push(day)
-
-	day++
-	date = new Date(date.getTime() + 24 * 60 * 60 * 1000)
-}
-
-const container = document.querySelector(".calendar")
-container.append(
-	...calendar.map((month, m) => {
+document.querySelector(".calendar").append(
+	...[...Array(12)].map((_, month) => {
 		const monthContainer = document.createElement("div")
 		monthContainer.className = "month"
 
 		const title = document.createElement("h2")
 		title.className = "title"
-		title.innerText = new Date(year, m, 2).toLocaleString("default", {
+		title.innerText = new Date(year, month, 2).toLocaleString("default", {
 			month: "long",
 		})
 
 		const grid = document.createElement("div")
 		grid.className = "grid"
 		grid.append(
-			...month.map((ofYear, ofMonth) => {
-				const ofWeek = new Date(year, m, ofMonth + 1).getUTCDay()
+			...[...Array(new Date(year, month + 1, 0).getDate())].map(
+				(_, ofMonth) => {
+					const ofWeek = new Date(year, month, ofMonth + 1).getUTCDay()
 
-				const cell = document.createElement("div")
-				cell.className = `day${ofWeek > 4 ? " weekend" : ""}`
-				cell.innerText = ofYear
-				return cell
-			})
+					const cell = document.createElement("div")
+					cell.className = `day${ofWeek > 4 ? " weekend" : ""}`
+					cell.innerText = ++day
+					return cell
+				}
+			)
 		)
 
 		monthContainer.append(title, grid)
